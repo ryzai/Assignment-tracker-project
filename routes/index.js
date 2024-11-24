3cacbbb28aa38d37970a334da38ce7d0a4985d4b
ryzai <99234031+ryzai@users.noreply.github.com>
2024-11-24 10:52:30 -0500

    Delete routes directory

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-//Home-Page Display
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.render('index', { title: 'Assignment Tracker' });
-});
-
-module.exports = router;
